721f560b542719a247a4c6ea94171eb4758676b1
xxharmony <qw875048684@gmail.com>
2023-11-12 08:08:46 +0000

    JF-7,test

--- build/sql/2_0_create_table_world_stats.sql
+++ build/sql/2_0_create_table_world_stats.sql
@@ -7,3 +7,16 @@ CREATE TABLE "user"
     type varchar(20) NOT NULL,
     CONSTRAINT id PRIMARY KEY (id)
 );
+CREATE TABLE "job_seeker_account"
+(
+    id uuid not null default uuid_generate_v4() primary key ,
+    ac_username varchar(50) not null unique,
+    ac_password varchar(50) not null,
+    nickname varchar(50) not null,
+    email varchar(50) not null,
+    phone varchar(20) not null,
+    firstname varchar(50) not null,
+    lastname varchar(50) not null,
+    gender varchar(10) not null
+
+);
---
+++ src/main/java/uk/ac/le/qasm/job/searching/api/controller/JobSeekerACController.java
@@ -0,0 +1,45 @@
+package uk.ac.le.qasm.job.searching.api.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uk.ac.le.qasm.job.searching.api.entity.JobSeekerAccount;
+import uk.ac.le.qasm.job.searching.api.exception.BaseException;
+import uk.ac.le.qasm.job.searching.api.usecase.CreateJobSeekerACUseCase;
+import uk.ac.le.qasm.job.searching.api.usecase.GetJobSeekerACUserCase;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+@RequestMapping("/JobSeekerAC")
+public class JobSeekerACController {
+    private final CreateJobSeekerACUseCase createJobSeekerACUseCase;
+    private final GetJobSeekerACUserCase getJobSeekerACUserCase;
+
+    public JobSeekerACController(CreateJobSeekerACUseCase createJobSeekerACUseCase, GetJobSeekerACUserCase getJobSeekerACUserCase) {
+        this.createJobSeekerACUseCase = createJobSeekerACUseCase;
+        this.getJobSeekerACUserCase = getJobSeekerACUserCase;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody JobSeekerAccount jobSeekerAccount) {
+        try {
+        return ResponseEntity.status(HttpStatus.CREATED).body(createJobSeekerACUseCase.create(jobSeekerAccount));
+        } catch (BaseException ex) {
+            return ResponseEntity.status(ex.getHttpStatus()).body(Map.of("message", ex.getDescription()));
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> get(@PathVariable("id") UUID id) {
+        try {
+            return ResponseEntity.ok(getJobSeekerACUserCase.getById(id));
+        } catch (BaseException ex) {
+            return ResponseEntity.status(ex.getHttpStatus()).body(Map.of("message", ex.getDescription()));
+        }
+    }
+
+}
