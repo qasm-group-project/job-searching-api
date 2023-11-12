6d18aefb5e60b26309f62e7647683480fb77f214
xxharmony <qw875048684@gmail.com>
2023-11-12 08:29:39 +0000

    JF-7,test

--- src/main/java/uk/ac/le/qasm/job/searching/api/entity/JobSeekerAccount.java
+++ src/main/java/uk/ac/le/qasm/job/searching/api/entity/JobSeekerAccount.java
@@ -1,64 +1,49 @@
 package uk.ac.le.qasm.job.searching.api.entity;
 
-
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import lombok.Data;
 
 import java.util.UUID;
 
 @Entity
-@Data
-@TableName("job_seeker_account")
 @Table(name = "job_seeker_account", schema = "job_searching")
 public class JobSeekerAccount {
     @Id
     @GeneratedValue
     @Column(name = "id")
     @JsonProperty(value = "id", access = JsonProperty.Access.READ_ONLY)
-    @TableField("id")
     private UUID id;
 
     @Column(name = "ac_username")
     @JsonProperty(value = "ac_username")
-    @TableField("ac_username")
     private String ac_username;
 
     @Column(name = "ac_password")
     @JsonProperty(value = "ac_password")
-    @TableField("ac_password")
     private String ac_password;
 
     @Column(name = "nickname")
     @JsonProperty(value = "nickname")
-    @TableField("nickname")
     private String nickname;
 
     @Column(name = "email")
     @JsonProperty(value = "email")
-    @TableField("email")
     private String email;
 
     @Column(name = "phone")
     @JsonProperty(value = "phone")
-    @TableField("phone")
     private String phone;
 
     @Column(name = "firstname")
     @JsonProperty(value = "firstname")
-    @TableField("firstname")
     private String firstname;
 
     @Column(name = "lastname")
     @JsonProperty(value = "lastname")
-    @TableField("lastname")
     private String lastname;
 
     @Column(name = "gender")
     @JsonProperty(value = "gender")
-    @TableField("gender")
     private String gender;
 
 
--- src/main/java/uk/ac/le/qasm/job/searching/api/repository/JobSeekerACRepository.java
+++ src/main/java/uk/ac/le/qasm/job/searching/api/repository/JobSeekerACRepository.java
@@ -1,10 +1,8 @@
 package uk.ac.le.qasm.job.searching.api.repository;
-import org.apache.ibatis.annotations.Mapper;
 import org.springframework.data.repository.CrudRepository;
 import uk.ac.le.qasm.job.searching.api.entity.JobSeekerAccount;
 
 import java.util.UUID;
 
-@Mapper
 public interface JobSeekerACRepository extends CrudRepository<JobSeekerAccount, UUID> {
 }
