539c481d522c89b845bf4e0adb4061d7f07652f1
xxharmony <qw875048684@gmail.com>
2023-11-15 21:15:28 +0000

    Merge https://github.com/qasm-group-project/job-searching-api into JF-7__rx
    # Conflicts:
    #	JobSeeker login and register with security

--- src/main/java/uk/ac/le/qasm/job/searching/api/service/impl/JSAuthServiceImpl.java
+++ src/main/java/uk/ac/le/qasm/job/searching/api/service/impl/JSAuthServiceImpl.java
@@ -10,6 +10,8 @@ import uk.ac.le.qasm.job.searching.api.entity.JobSeeker;
 import uk.ac.le.qasm.job.searching.api.mapper.JobSeekerMapper;
 import uk.ac.le.qasm.job.searching.api.request.AuthenticationRequest;
 import uk.ac.le.qasm.job.searching.api.service.JSAuthService;
+import uk.ac.le.qasm.job.searching.api.usecase.CheckJobSeekerACUsernameUserCase;
+import uk.ac.le.qasm.job.searching.api.usecase.CreateJobSeekerACUseCase;
 
 import java.util.HashMap;
 @Service
@@ -17,65 +19,63 @@ public class JSAuthServiceImpl implements JSAuthService {
     private final JobSeekerMapper jobSeekerMapper;
     private final JwtService jwtService;
     private final PasswordEncoder passwordEncoder;
-
-
-
-    public JSAuthServiceImpl(JobSeekerMapper jobSeekerMapper, JwtService jwtService, PasswordEncoder passwordEncoder) {
+    private final CreateJobSeekerACUseCase createJobSeekerACUseCase;
+    private final CheckJobSeekerACUsernameUserCase checkJobSeekerACUsernameUserCase;
+    public JSAuthServiceImpl(JobSeekerMapper jobSeekerMapper, JwtService jwtService, PasswordEncoder passwordEncoder, CreateJobSeekerACUseCase createJobSeekerACUseCase, CheckJobSeekerACUsernameUserCase checkJobSeekerACUsernameUserCase) {
         this.jobSeekerMapper = jobSeekerMapper;
         this.jwtService = jwtService;
         this.passwordEncoder = passwordEncoder;
+        this.createJobSeekerACUseCase = createJobSeekerACUseCase;
+        this.checkJobSeekerACUsernameUserCase = checkJobSeekerACUsernameUserCase;
     }
 
     @Override
     public ResponseEntity<Object> register(JobSeeker jobSeekerAccount) {
         HashMap<String,Object> res = new HashMap<>();
-        QueryWrapper<JobSeeker> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("username",jobSeekerAccount.getUsername());
-        if (jobSeekerMapper.exists(queryWrapper)){
+
+        if (checkJobSeekerACUsernameUserCase.Check(jobSeekerAccount.getUsername())){
             res.put("message","username already exist!");
             res.put("status", HttpStatus.FORBIDDEN.value());
             return new ResponseEntity<>(res, HttpStatus.FORBIDDEN);
         }else {
-            if (jobSeekerMapper.insert(jobSeekerAccount)>=1){
-//                var jobSeeker = JobSeeker.builder()
-//                        .username(jobSeekerAccount.getUsername())
-//                        .password(passwordEncoder.encode(jobSeekerAccount.getPassword()))
-//                        .gender(jobSeekerAccount.getGender())
-//                        .email(jobSeekerAccount.getEmail())
-//                        .phone(jobSeekerAccount.getPhone())
-//                        .firstname(jobSeekerAccount.getFirstname())
-//                        .lastname(jobSeekerAccount.getLastname())
-//                        .nickname(jobSeekerAccount.getNickname())
-//                        .build();
+                var jobSeeker = JobSeeker.builder()
+                        .username(jobSeekerAccount.getUsername())
+                        .password(passwordEncoder.encode(jobSeekerAccount.getPassword()))
+                        .gender(jobSeekerAccount.getGender())
+                        .email(jobSeekerAccount.getEmail())
+                        .phone(jobSeekerAccount.getPhone())
+                        .firstname(jobSeekerAccount.getFirstname())
+                        .lastname(jobSeekerAccount.getLastname())
+                        .nickname(jobSeekerAccount.getNickname())
+                        .build();
 //                var jwtToken = jwtService.generateToken(jobSeeker);
-                res.put("message","register success!");
-                res.put("status",HttpStatus.OK.value());
 //                res.put("token",jwtToken);
-            return new ResponseEntity<>(res, HttpStatus.OK);
-            }else {
-                res.put("message","system error");
-                res.put("status",HttpStatus.INTERNAL_SERVER_ERROR.value());
-                return new ResponseEntity<>(res,HttpStatus.INTERNAL_SERVER_ERROR);
-            }
+            return ResponseEntity.status(HttpStatus.CREATED).body(createJobSeekerACUseCase.create(jobSeeker));
         }
-
     }
 
     @Override
     public ResponseEntity<?> login(AuthenticationRequest authenticationRequest) {
         HashMap<String,Object> res = new HashMap<>();
         QueryWrapper<JobSeeker> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("username",authenticationRequest.getUsername())
-                    .eq("password",authenticationRequest.getPassword());
+        queryWrapper.eq("username",authenticationRequest.getUsername());
 
         var jobseeker = jobSeekerMapper.selectOne(queryWrapper);
         if (jobseeker!=null){
-            res.put("message","Login success");
-            res.put("status",HttpStatus.OK.value());
-            res.put("token",jwtService.generateToken(jobseeker));
-            return new ResponseEntity<>(res,HttpStatus.OK);
+            if (passwordEncoder.matches(authenticationRequest.getPassword(),jobseeker.getPassword())){
+                res.put("message","Login success");
+                res.put("status",HttpStatus.OK.value());
+                res.put("token",jwtService.generateToken(jobseeker));
+                return new ResponseEntity<>(res,HttpStatus.OK);
+            }else {
+                res.put("message","wrong password");
+                res.put("status",HttpStatus.UNAUTHORIZED.value());
+                return new ResponseEntity<>(res,HttpStatus.UNAUTHORIZED);
+            }
         }else {
-            return new ResponseEntity<>(res,HttpStatus.INTERNAL_SERVER_ERROR);
+            res.put("message","username does not exist");
+            res.put("status",HttpStatus.NOT_FOUND.value());
+            return new ResponseEntity<>(res,HttpStatus.NOT_FOUND);
         }
 
     }
