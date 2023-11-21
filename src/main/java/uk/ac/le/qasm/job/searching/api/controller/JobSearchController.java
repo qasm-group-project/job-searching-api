7e75e8fa691e7cf1217d41f9ef1dee7f86baba18
xxharmony <qw875048684@gmail.com>
2023-11-21 13:41:36 +0000

    Merge https://github.com/qasm-group-project/job-searching-api into JF-7__rx
    # Conflicts:
    #	jobSearch

--- src/main/java/uk/ac/le/qasm/job/searching/api/controller/JobSearchController.java
+++ src/main/java/uk/ac/le/qasm/job/searching/api/controller/JobSearchController.java
@@ -24,8 +24,6 @@ public class JobSearchController {
 
     @GetMapping
     public ResponseEntity<?> searchJob(){
-        SecurityContextHolder.getContext().getAuthentication();
-//        Authentication authentication =
         return jobSearchService.searchAll();
     }
 
