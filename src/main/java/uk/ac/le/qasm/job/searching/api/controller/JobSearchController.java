d061ed31e44a5ff0d3f3fdb78e11bfbce8a7c112
xxharmony <qw875048684@gmail.com>
2023-11-21 13:42:24 +0000

    Merge https://github.com/qasm-group-project/job-searching-api into JF-7__rx
    # Conflicts:
    #	jobSearch

--- src/main/java/uk/ac/le/qasm/job/searching/api/controller/JobSearchController.java
+++ src/main/java/uk/ac/le/qasm/job/searching/api/controller/JobSearchController.java
@@ -2,12 +2,9 @@ package uk.ac.le.qasm.job.searching.api.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uk.ac.le.qasm.job.searching.api.entity.JobSeeker;
 import uk.ac.le.qasm.job.searching.api.service.JobSearchService;
 
 @Slf4j
